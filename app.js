a1eed28472100ff766741e30a0284a0486fef31c
rahul-g5125 <rahul.h.gandla@gmail.com>
2023-12-20 17:15:21 +0530

    changing solutions to initiatives

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.get("/programs/:programId", (req, res) => {
 });
 
 app.get("/", (req, res) => {
-  res.render("home", { programs: programs, solutions: solutions });
+  res.render("home", { programs: programs, initiatives: solutions });
 });
 
 app.get("/legal", (req, res) => {
