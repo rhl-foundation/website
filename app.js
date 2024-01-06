c673a68a8d1b4ff21130081bf9a7c4e5de386ec8
Prathamesh532 <prathameshkalekar0532@gmail.com>
2024-01-07 00:45:04 +0530

    Add a Partner Page

--- app.js
+++ app.js
@@ -124,7 +124,7 @@ app.get("/:page", (req, res) => {
   });
 });
 
-// partners page
+// Add partners page
 app.get("/get-involved/partner", (req, res) => {
   res.render("partner");
 });
@@ -350,6 +350,6 @@ app.post("/subscribe/newsletter", (req, res) => {
 
 const key_id = process.env.RAZORPAY_KEY_ID;
 
-app.listen(5000, () => {
+app.listen(3000, () => {
   console.log("Successfully started listening on port " + port);
 });
